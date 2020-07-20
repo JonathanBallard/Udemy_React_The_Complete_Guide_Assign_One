33a89d53476c11dc08f6c36a72c2323d354b3e23
Jonathan Ballard <asgardschosen@gmail.com>
2020-07-19 19:29:13 -0700

    first commit

---
+++ src/components/UserInput.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const UserInput = props => {
+    return (
+        <div className='UserInput'>
+            <input type='text' value='input'></input>
+
+        </div>
+    )
+
+
+};
+
+
+export default UserInput;
---
+++ src/components/UserOutput.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const UserOutput = props => {
+    return (
+        <div className='UserOutput'>
+            <h5 className='username'>{ props.username }</h5>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quibusdam quod sunt dicta officia nulla voluptas eos vero. Impedit facilis tempora quisquam eveniet consequatur enim aut!</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima ratione sed modi labore quod consequuntur, recusandae nam velit animi est omnis maxime? Quisquam et incidunt molestiae nam quam, voluptate quaerat provident eum ad officia, laboriosam delectus, totam temporibus commodi magni quos! Quod quaerat sunt vero voluptatibus quisquam quis sint eos!</p>
+        </div>
+
+    )
+
+};
+
+export default UserOutput;
